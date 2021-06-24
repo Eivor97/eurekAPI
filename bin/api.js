8763d5e3d39ff17a92bc440109c3b6768b36677d
andromano <romano.andrea.1993@gmail.com>
2021-06-24 19:28:48 +0000

    aggiunto CORS

--- bin/api.js
+++ bin/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var cors = require('cors');
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,7 @@ const apiRouter = require('../routes/api');
 const db = require('../db/middleware');
 db.init();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
 
